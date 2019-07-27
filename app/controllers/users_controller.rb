551b584d2420fd688141f2ac7d4729178cf2e8b8
Meo So <meowsoso@gmail.com>
2019-07-27 14:59:30 +1000

    completed user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,36 @@
 class UsersController < ApplicationController
+  before_action :check_for_login, :only => [:edit, :update]
+  before_action :check_for_admin, :only => [:index]
+  
   def new
+    @user = User.new
+  end
+  
+    def index
+      @users = User.all 
+    end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      redirect_to root_path
+      # session[:user_id] = @user.id 
+    else 
+      render :new
+    end
+  end
+
+  def edit
+    @user = @current_user
+  end
+
+  def update
+    @current_user.update user_params
+    redirect_to root_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,22 @@
+# == Route Map
+#
+#   Prefix Verb   URI Pattern          Controller#Action
+#     root GET    /                    pages#home
+#    users GET    /users(.:format)     users#index
+#          POST   /users(.:format)     users#create
+# new_user GET    /users/new(.:format) users#new
+#     user PATCH  /users/:id(.:format) users#update
+#          PUT    /users/:id(.:format) users#update
+#    login GET    /login(.:format)     session#new
+#          POST   /login(.:format)     session#create
+#          DELETE /login(.:format)     session#destroy
+
 Rails.application.routes.draw do
+  root :to => 'pages#home'
+  resources :users, :only => [:new, :create, :update, :index]
+  get '/users/edit' => 'users#edit', :as => :edit_user
+  
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
 end
